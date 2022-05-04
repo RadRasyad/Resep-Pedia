84e49a617b838058309e56ce5e8ebdcbb4e59568
Rafi Dany Rasyad <rafidanyr21@gmail.com>
2022-05-04 08:25:45 +0700

    Recipe Adapter, Dependencies Glide diganti dengan Picasso.

--- app/src/main/java/com/rpl/reseppedia/model/remote/response/RecipeResponse.java
+++ app/src/main/java/com/rpl/reseppedia/model/remote/response/RecipeResponse.java
@@ -13,6 +13,97 @@ public class RecipeResponse {
     private String porsi;
     private String kesulitan;
     private String deksripsi;
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    private String foto;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPenulis() {
+        return penulis;
+    }
+
+    public void setPenulis(String penulis) {
+        this.penulis = penulis;
+    }
+
+    public String getDirilis() {
+        return dirilis;
+    }
+
+    public void setDirilis(String dirilis) {
+        this.dirilis = dirilis;
+    }
+
+    public String getWaktu() {
+        return waktu;
+    }
+
+    public void setWaktu(String waktu) {
+        this.waktu = waktu;
+    }
+
+    public String getPorsi() {
+        return porsi;
+    }
+
+    public void setPorsi(String porsi) {
+        this.porsi = porsi;
+    }
+
+    public String getKesulitan() {
+        return kesulitan;
+    }
+
+    public void setKesulitan(String kesulitan) {
+        this.kesulitan = kesulitan;
+    }
+
+    public String getDeksripsi() {
+        return deksripsi;
+    }
+
+    public void setDeksripsi(String deksripsi) {
+        this.deksripsi = deksripsi;
+    }
+
+    public ArrayList<String> getBahan() {
+        return bahan;
+    }
+
+    public void setBahan(ArrayList<String> bahan) {
+        this.bahan = bahan;
+    }
+
+    public ArrayList<String> getCaraMasak() {
+        return caraMasak;
+    }
+
+    public void setCaraMasak(ArrayList<String> caraMasak) {
+        this.caraMasak = caraMasak;
+    }
+
     private ArrayList<String> bahan;
     private ArrayList<String> caraMasak;
 }
---
+++ app/src/main/java/com/rpl/reseppedia/ui/home/RecipeAdapter.java
@@ -0,0 +1,58 @@
+package com.rpl.reseppedia.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rpl.reseppedia.R;
+import com.rpl.reseppedia.databinding.RecipeRowBinding;
+import com.rpl.reseppedia.model.remote.response.RecipeResponse;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+
+    private ArrayList<RecipeResponse> listRecipe;
+
+    public RecipeAdapter(ArrayList<RecipeResponse> listRecipe) {
+        this.listRecipe = listRecipe;
+    }
+
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecipeRowBinding binding = RecipeRowBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new RecipeViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+
+        RecipeResponse recipe = listRecipe.get(position);
+
+        holder.bind(recipe);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listRecipe.size();
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+
+        private final RecipeRowBinding binding;
+
+        public RecipeViewHolder(RecipeRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        void bind(RecipeResponse recipe) {
+            binding.tvRecipeName.setText(recipe.getName());
+            Picasso.get().load(recipe.getFoto()).placeholder(R.drawable.placeholder_img).error(R.drawable.ic_error).fit().into(binding.ivRecipe);
+        }
+    }
+}
